37b5aa803421e16656cc677b7a5d5dc54e60797c
gitsw1tch <david2uz178@gmail.com>
2023-07-30 15:56:06 +0500

    Initial Commit

---
+++ main.py
@@ -0,0 +1,55 @@
+import telebot
+import tools
+from tools import get_str_from as gsf
+from configs import TOKEN
+
+
+bot = telebot.TeleBot(TOKEN)
+
+text_get_ip = 'Enter IP address (i.e. 192.168.0.1)'
+text_get_mask = 'Choose subnet mask'
+
+temp = {} #{message.from_user.id: {'ip': iplist} -> to save ip in next handler
+
+@bot.message_handler(commands = ['start'])
+def start(message):
+	bot.send_message(message.chat.id, text_get_ip)
+
+@bot.message_handler(content_types = ['text'])
+def get_ip(message):
+	try:
+		ip = message.text.split('.')    #'1.1.1.1' -> ['1', '1', '1', '1']
+		ip = list(map(int, ip))         #['1', '1', '1', '1'] -> [1, 1, 1, 1]
+		if tools.check_ip(ip):
+			request_ip = {message.from_user.id: {'ip': ip}}
+			temp.update(request_ip)
+			msg = bot.send_message(message.chat.id, text_get_mask, reply_markup = tools.get_mask_markup())
+			bot.register_next_step_handler(msg, get_mask)	
+		else:
+			bot.reply_to(message, 'Error! ' + text_get_ip)
+	except Exception:
+		bot.reply_to(message, 'Error! ' + text_get_ip)
+	
+def get_mask(message):
+	try:
+		mask = message.text.partition('/')[0].strip()     #'255.255.255.255 /32' -> '255.255.255.255'
+		if tools.check_mask(mask):
+			mask = list(map(int, mask.split('.')))        #'255.255.255.255' -> [255, 255, 255, 255]               
+			ip = temp[message.from_user.id]['ip']
+			network_addr = [x & y for x, y in zip(ip, mask)]			
+			inversed_mask = [255 - x for x in mask]
+			broadcast_addr = [x + y for x, y in zip(network_addr, inversed_mask)]
+			content = ('IP: {}\n'
+						'Subnet Mask: {}\n'
+						'Network Address: {}\n'
+						'Broadcast Address: {}\n').format(gsf(ip), gsf(mask), gsf(network_addr), gsf(broadcast_addr))
+			bot.send_message(message.chat.id, content)
+			temp.pop(message.from_user.id) #clear temp
+		else:
+			msg = bot.reply_to(message, 'Error! ' + text_get_mask, reply_markup = tools.get_mask_markup())
+			bot.register_next_step_handler(msg, get_mask)
+	except Exception:
+		msg = bot.reply_to(message, 'Error! ' + text_get_mask, reply_markup = tools.get_mask_markup())
+		bot.register_next_step_handler(msg, get_mask)
+		
+bot.infinity_polling()
---
+++ tools.py
@@ -0,0 +1,41 @@
+from telebot.types import ReplyKeyboardMarkup, KeyboardButton
+
+all_mask = ['0.0.0.0', '128.0.0.0', '192.0.0.0',
+			'224.0.0.0', '240.0.0.0', '248.0.0.0', 
+			'252.0.0.0', '254.0.0.0', '255.0.0.0',
+			'255.128.0.0', '255.192.0.0', '255.224.0.0',
+			'255.240.0.0', '255.248.0.0', '255.252.0.0',
+			'255.254.0.0', '255.255.0.0', '255.255.128.0',
+			'255.255.192.0', '255.255.224.0', '255.255.240.0',
+			'255.255.248.0', '255.255.252.0', '255.255.254.0',
+			'255.255.255.0', '255.255.255.128', '255.255.255.192',
+			'255.255.255.224', '255.255.255.240', '255.255.255.248',
+			'255.255.255.252', '255.255.255.254', '255.255.255.255']
+
+def check_ip(ip):
+	if len(ip) == 4:
+		for i in ip:
+			if i < 0 or i > 255:
+				return False
+	return True
+	
+def check_mask(mask):
+	if mask in all_mask:
+		return True
+	return False
+
+def get_str_from(src_list):
+	string = '.'.join(str(x) for x in src_list)
+	return string
+	
+def get_mask_markup():
+	mask_markup = ReplyKeyboardMarkup(row_width = 1)
+	i = 0
+	while i < len(all_mask):
+		button_content = all_mask[i] + ' /' + str(i)
+		mask_markup.add(button_content)
+		i += 1
+	return mask_markup
+
+
+	
